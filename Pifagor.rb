8d165a99471997782cf59a1c03188f6f4da8ce32
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-06-20 23:57:41 +0600

    Update Pifagor.rb

--- Pifagor.rb
+++ Pifagor.rb
@@ -1,22 +1,19 @@
 puts "Napishite znacheniya"
 
-a = (gets.chomp).to_i
+a = (gets.chomp).to_f
 puts a
 
-b = (gets.chomp).to_i
+b = (gets.chomp).to_f
 puts b
 
-c = (gets.chomp).to_i
+c = (gets.chomp).to_f
 puts c
 
 if 
-	(c ** 2) == (a ** 2) + (b ** 2)
-    puts "Pifagor theory"
-elsif 
-	c < a + b
-	puts "None"
+	(c **2) == (a **2) + (b **2)
+    puts "Pryamougolniy"
 else
-	c > a + b
-	puts "None 2"
+	a = b = c
+    puts "Ravnostoronniy i Ravnobedrenniy" 
 end
 
