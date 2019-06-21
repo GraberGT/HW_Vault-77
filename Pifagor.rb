1621e56da0b588161144dbdd6a1fa912a010c991
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-06-22 00:11:03 +0600

    Update Pifagor.rb

--- Pifagor.rb
+++ Pifagor.rb
@@ -1,19 +1,28 @@
 puts "Napishite znacheniya"
 
-a = (gets.chomp).to_f
+a = gets.chomp.to_f 
 puts a
 
-b = (gets.chomp).to_f
+b = gets.chomp.to_f 
 puts b
 
-c = (gets.chomp).to_f
+c = gets.chomp.to_f 
 puts c
 
-if 
-	(c **2) == (a **2) + (b **2)
-    puts "Pryamougolniy"
-else
-	a = b = c
-    puts "Ravnostoronniy i Ravnobedrenniy" 
-end
 
+arr = [a, b, c].sort!
+
+k1 = [0]
+k2 = [1]
+hypotenuse = [2]
+
+if    hypotenuse**2 == k1**2 + k2**2
+puts "Pryamougolniy"
+
+elsif k1 = k2
+puts "Ravnobedrenniy"
+
+elsif hypotenuse = k1 = k2
+puts "Ravnostoronniy" 
+	
+end
