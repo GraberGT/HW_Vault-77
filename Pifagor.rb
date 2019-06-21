571f9c6cd12d3c3640e60dcdc2a5ae8f151c317c
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-06-22 01:10:38 +0600

    Update Pifagor.rb

--- Pifagor.rb
+++ Pifagor.rb
@@ -10,12 +10,12 @@ c = gets.chomp.to_f
 puts c
 
 
-arr = [a, b, c].sort!
+arr = [a, b, c].sort!      или так     my_array = [a, b, c].sort!                     
 
-k1 = [0]
-k2 = [1]
-hypotenuse = [2]
-
+k1 = [0]                               my_array[0] = k1
+k2 = [1]                               my_array[1] = k2
+hypotenuse = [2]                       my_array[2] = hypotenuse
+ 
 if    hypotenuse**2 == k1**2 + k2**2
 puts "Pryamougolniy"
 
