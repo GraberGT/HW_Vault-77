beb9ee3d82291aee378692cce6d37df7559d0c55
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-06-24 19:28:32 +0600

    Update Months.rb

--- Months.rb
+++ Months.rb
@@ -1,5 +1,3 @@
 hash = {January: 31, February: 28, March: 31, April: 30, May: 31, June: 30, July: 31, August: 30, September: 30, October: 31, November: 30, December: 31}
 
-hash.key(30)
-
-puts hash.key(30)
+hash.each {|months, days| puts months if days == 30}
