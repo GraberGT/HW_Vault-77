4645e3c82df9f16718127a29b16b16a53db8e45b
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-06-28 20:12:20 +0600

    Update years.rb

--- years.rb
+++ years.rb
@@ -9,7 +9,7 @@ year = gets.chomp.to_i
 
 days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-if year % 4 == 0 && year % 400 == 0 && year % 100 != 0
+if year % 4 == 0 || year % 400 == 0 || year % 100 != 0
 days[1] = 29
 
 end
