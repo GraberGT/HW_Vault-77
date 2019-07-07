663e538b28bcbdec23ae0b77afe7b73d1e79f517
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-07-08 01:18:34 +0600

    Add files via upload

---
+++ zadac4i/zadacha2_2.rb
@@ -0,0 +1,15 @@
+arr1 = [1, 2, 4, 5, 6, 7]
+
+def remove_duplicate(arr1)
+
+arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+uniques = (arr1 - arr2)
+duplicates = (arr1 - arr2)
+
+puts uniques
+puts duplicates
+
+end
+
+remove_duplicate(arr1)
