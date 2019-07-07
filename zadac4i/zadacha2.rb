b01d4b07ad1abb15b0d6a99cde0134cdda715132
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-07-08 01:17:54 +0600

    Add files via upload

---
+++ zadac4i/zadacha2.rb
@@ -0,0 +1,16 @@
+arr1 = [1, 2, 4, 5, 6, 7]
+
+def remove_duplicate(arr1)
+
+    arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    puts (arr1 | arr2).sort
+
+    puts (arr1 & arr2).sort
+
+end
+
+remove_duplicate(arr1)
+
+
+
