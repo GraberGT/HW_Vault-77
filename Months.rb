87bf0b548f9ffdff1cc42ce725bcec2be015384f
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-07-09 00:24:43 +0600

    delete months.rb

--- Months.rb
+++
@@ -1,3 +0,0 @@
-hash = {January: 31, February: 28, March: 31, April: 30, May: 31, June: 30, July: 31, August: 30, September: 30, October: 31, November: 30, December: 31}
-
-hash.each {|months, days| puts months if days == 30}
