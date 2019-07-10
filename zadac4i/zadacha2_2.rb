b1b1cc5fe4b1e27d1228bd1c2caa314fbdee4497
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-07-10 19:52:13 +0600

    Update zadacha2_2.rb

--- zadac4i/zadacha2_2.rb
+++ zadac4i/zadacha2_2.rb
@@ -1,11 +1,11 @@
 arr1 = [1, 2, 4, 5, 6, 7]
 
-def remove_duplicate(arr1)
-
 arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-uniques = (arr1 - arr2)
-duplicates = (arr1 - arr2)
+def remove_duplicate(arr1, arr2)
+
+uniques = (arr2 - arr1)
+duplicates = (arr2 - arr1)
 
 puts uniques
 puts duplicates
