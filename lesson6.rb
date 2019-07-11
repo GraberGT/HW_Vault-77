e04f81da23152e5f41cea10a1815e8eb4a4b9365
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-07-12 02:36:06 +0600

    Update lesson6.rb

--- lesson6.rb
+++ lesson6.rb
@@ -1,30 +1,29 @@
 def ask(string)
 
 	puts "What do you want to know? Time, Date, Day, Remaining days, Remaining weeks."
-    a = gets.chomp.capitalize
+        a = gets.chomp.capitalize
 
 	puts "Incorrect Request" if string.class != a
 
     case a
 
-		when "Time" 
-		puts Time.now.strftime("%H:%M") if a == "Time"
+	when "Time" 
+	puts Time.now.strftime("%H:%M") if a == "Time"
 
     	when "Date"
     	puts Time.now.strftime("%d:%m:%y") if a == "Date"
 		
-		when "Day"
-	    puts Time.now.strftime("%A") if a == "Day"
+	when "Day"
+	puts Time.now.strftime("%A") if a == "Day"
 
-		when "Remaining days"
-        
-		puts Time.now.strftime("%j") if a == "Remaining days"
+	when "Remaining days"
+        puts Time.now.strftime("%j") if a == "Remaining days"
 
-		when "Remaining weeks"
-		puts Time.now.strftime("%W") if a == "Remaining weeks"	
+	when "Remaining weeks"
+	puts Time.now.strftime("%W") if a == "Remaining weeks"	
         
         end
 
-	end
+end
 
 ask('string')
