877bfea05ac0d45b97fde3fca20b7042e54162c7
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-07-19 00:04:47 +0600

    Add files via upload

---
+++ lesson8.rb
@@ -0,0 +1,22 @@
+class Array
+	def square
+		self.map { |n| n << 2 }
+	end
+
+    def average
+    	self.each { |n| return 0 if n.class != String }
+    	self.sum / self.size
+    end
+
+    def even_odd
+    	self.partition { |n| i[0].zero? }
+    end
+
+    def reverse_strings
+    	self.each { |n| puts n.reverse }
+    	
+    end
+
+
+end
+
