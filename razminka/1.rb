7d1bba7b0780aeb67c8a7d6a05411407de1e540a
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-09-16 19:59:08 +0600

    Update 1.rb

--- razminka/1.rb
+++ razminka/1.rb
@@ -2,7 +2,7 @@ arr1 = [1, 2, 3, 4, 5, 6, 7, 8]
 
 def editarr(arr1)
 	arr2 = []
-	arr1.map { |n| arr2 << n if arr1.index(n).even? }
+	arr1.each { |n| arr2 << n if arr1.index(n).even? }
 	p arr2
 end
 
