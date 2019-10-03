5d8505fdcb8a761cd985eae45137a807e41d6d82
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-10-04 00:11:49 +0600

    Add files via upload

---
+++ lesson_14/station.rb
@@ -0,0 +1,24 @@
+require_relative 'passanger_train'
+require_relative 'cargo_train'
+
+class Station
+  attr_reader :name
+  attr_reader :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def join_train(train)
+    @trains << train
+  end
+
+  def trains_type(type)
+    @trains.select{|train| train.type == type}
+  end
+
+  def send_train(train)
+    @trains.delete(train)
+  end
+end
