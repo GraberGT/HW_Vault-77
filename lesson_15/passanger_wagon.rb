abc8ee2d0fcf44373d118b7adc9e94f3d9ef2bf6
Yuriy Mikhaylenko <51834517+GraberGT@users.noreply.github.com>
2019-10-09 19:16:16 +0600

    Add files via upload

---
+++ lesson_15/passanger_wagon.rb
@@ -0,0 +1,7 @@
+class Passanger_wagon
+  attr_reader :pass_seats
+
+   def initialize(pass_seats)
+     @pass_seats = pass_seats
+   end
+end
---
+++ lesson_15/train.rb
@@ -0,0 +1,66 @@
+require_relative 'route'
+
+class Train
+  attr_reader :number, :type
+
+  @@trains = {}  
+
+  def self.all
+    @@trains
+  end
+
+  def initialize(number)
+    @number = number
+    @quantity_wagon = []
+    @@trains[number] = self
+  end
+
+  def self.find(number)
+    all[number]
+  end
+
+  
+
+  def station_route(route)
+    @route = route
+    @current_station = @route.stations.first
+    @current_station.join_train(self)
+  end
+
+  def add_wagon(wagon)
+    return "Error" if wagon.type != @type
+    @amount_wagon << wagon
+  end
+
+  def current_station
+    @current_station
+  end
+
+  def next_station
+    @route.stations[current_station_index + 1]
+  end
+
+  def previous_station
+     @route.stations[current_station_index - 1]
+  end
+
+  def go_tonext_station
+    return if @current_station == @route.stations.last
+    move(next_station)
+  end
+
+  def go_toback_station
+    return if @current_station == @route.stations.first
+    move(previous_station)
+  end
+
+  def current_station_index
+    @route.stations.index(@current_station)
+  end
+
+    def move(station)
+    return unless @current_station && @route
+    @current_station.send_train(self)
+    @current_station = station
+    @current_station.join_train(self)
+  end
