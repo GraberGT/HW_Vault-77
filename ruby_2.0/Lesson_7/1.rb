fc442cd80fa5b0e0724ba6ceb7a81e5dbaf59b25
GraberGT <rogerpluscom@gmail.com>
2020-04-20 00:09:00 +0600

    updhwv77

---
+++ ruby_2.0/Lesson_7/1.rb
@@ -0,0 +1,20 @@
+def score_throws(radiuses)
+  return 0 if radiuses.empty?
+
+  arr = []
+  radiuses.each do |i|
+    radius = i.to_f
+
+    arr << 0  if radius > 10
+    arr << 10 if radius < 5
+    arr << 5  if radius >= 5 && radius <= 10
+  end
+
+  score = arr.sum
+  score += 100 if radiuses.all? { |i| i < 5}
+
+  puts "score: #{score}"
+  
+end
+
+score_throws([1, 2, 3, 6, 7, 9, 11, 13])
