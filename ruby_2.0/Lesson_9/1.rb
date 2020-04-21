a4bfba5ac45922964c141a3504fcb5824f5f4093
GraberGT <rogerpluscom@gmail.com>
2020-04-21 16:46:09 +0600

    hw_v77

---
+++ ruby_2.0/Lesson_9/1.rb
@@ -0,0 +1,23 @@
+def vendor(code, price, quantity)
+  items.each do |n|
+
+    if n[:code] == code && [:quantity] == 0
+      puts "#{n[:name]}, end"
+
+    elsif
+
+    end
+
+
+
+
+items = [{:name=>"Snickers", :code=>"A01", :quantity=>10, :price=>250},
+
+  {:name=>"Pepsi", :code=>"A02", :quantity=>5, :price=>350},
+
+  {:name=>"Orange Juice", :code=>"A03", :quantity=>10, :price=>400},
+
+  {:name=>"Bon Aqua", :code=>"A04", :quantity=>7, :price=>120}, 
+
+  {:name=>"Bounty", :code=>"A05", :quantity=>10, :price=>270}]
+
