5c02815244d6e44ca074cc92af905e533ccb8a39
GraberGT <rogerpluscom@gmail.com>
2020-06-05 19:41:32 +0600

    upd

---
+++ ruby_2.0/Lesson_15/passanger_train.rb
@@ -0,0 +1,22 @@
+require_relative 'station'
+require_relative 'train'
+require_relative 'passanger_wagon'
+
+class Passanger_train < Train
+  attr_reader :number, :amount_wagon
+
+  def initialize(number)
+    super(number)
+    @amount_wagon = []
+  end
+
+  def add_wagon(wagon)
+    return "Error" if wagon.class != Passanger_Wagon
+    @amount_wagon << wagon
+  end
+
+  def delete_wagon(wagon)
+    return "Error" if wagon.class != Passanger_Wagon
+    @wagons_amount.delete(wagon)
+  end
+end
---
+++ ruby_2.0/Lesson_15/route.rb
@@ -0,0 +1,27 @@
+require_relative 'station'
+
+class Route
+  attr_reader :stations
+
+  @@routes = {}
+
+  def self.all
+    @@routes
+  end
+
+  def initialize(first_station, second_station)
+    @first_s = first_station
+    @second_s = second_station
+    @stations = [@first_s, @second_s]
+    @@routes[self] = self.stations
+  end
+
+  def add_station(station)
+    @stations.insert(-2, station)
+  end
+
+  def delete_station(station)
+    return "Error" if station == @stations.first || @stations.last
+    @stations.delete(station)
+  end
+end
