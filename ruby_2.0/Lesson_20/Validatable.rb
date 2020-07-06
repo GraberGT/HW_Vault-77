cbacbec600f9ea737e96cf9554bc9a49bfbf9912
GraberGT <rogerpluscom@gmail.com>
2020-07-07 01:08:00 +0600

    upd

--- ruby_2.0/Lesson_20/Validatable.rb
+++ ruby_2.0/Lesson_20/Validatable.rb
@@ -9,11 +9,11 @@ module Validator
   end
 
   def validate_type_train
-    raise "Invalid type" unless type cargo_train.empty?|| passenger_train.empty
+    raise "Invalid type" unless type cargo_train.empty?|| passenger_train.empty?
   end
 
   def validate_volume
-    raise "Invalid volume" unless volume.max <= 10
+    raise "Invalid volume" unless volume.max >= 10
   end
 
 end
