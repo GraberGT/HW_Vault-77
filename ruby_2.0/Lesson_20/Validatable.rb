30652e9026e833c6937e2b8d984e021ab572a191
GraberGT <rogerpluscom@gmail.com>
2020-07-06 23:06:12 +0600

    ls_20

---
+++ ruby_2.0/Lesson_20/Validatable.rb
@@ -0,0 +1,15 @@
+module Validator
+  private
+  
+  def valid_number
+    raise "Invalid number" unless number[/^\d+$/]
+  end
+
+  def valid_name
+    raise "Invalid name" unless name.object.is_a
+  end
+
+  def 
+    
+  end
+
