8b9bf10e0c6e8753ee1e11120e8d474f839ab5e2
GraberGT <rogerpluscom@gmail.com>
2020-08-05 23:10:41 +0600

    upd

---
+++ ruby_2.0/Lesson_26/api.rb
@@ -0,0 +1,5 @@
+class Api < Sinatra::Base
+
+
+
+end
---
+++ ruby_2.0/Lesson_26/station.rb
@@ -0,0 +1,18 @@
+class Station 
+  include Mongoid::Documet
+  field :name, type: String
+  field :trains, type: Array, default: []
+
+  validates :name, presence: true
+
+  index({name: 1}, {unique: true, name: 'name_index'})
+
+  def join(train)
+    push(trains: train) unless trains.include?(train)
+  end
+
+  def delete(train)
+    pull(trains: train)
+  end
+  
+end
