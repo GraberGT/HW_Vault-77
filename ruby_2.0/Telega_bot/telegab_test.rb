bc7c521d9a5c1bf429be41bbd797d22f2e12c8d4
GraberGT <rogerpluscom@gmail.com>
2020-08-08 22:31:11 +0600

    upd

---
+++ ruby_2.0/Telega_bot/telegab_test.rb
@@ -0,0 +1,53 @@
+require 'telegram/bot'
+token = 'token'
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.sendMessage(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when 'Time table'
+      bot.api.time_table
+    when 'Buy ticket'
+      bot.api.buy_ticket
+    when 'Buying tickets'
+      bot.api.tickets
+    end
+  end
+end
+
+
+
+def time_table
+  puts stations #stations = []
+end
+
+def buy_ticket
+  puts 'Enter your name'
+  @name = gets.chomp.to_s
+  puts 'Enter your sir name'
+  @sname = gets.chomp.to_s
+  puts "Enter your date of birth"
+  @dateb = gets.chomp.to_s
+  puts 'Enter your passport number'
+  @numpas = gets.chomp.to_s
+end
+
+def tikcets
+  puts tickets #tickets = []
+end
+
+
+
+
+
+
+class Client
+  field :name, type: String
+  field :sname, type: String
+  field :dateb, type: String
+  field :numpas, type: String
+
+end
+
+class Ticket
+  field :name, :sname, :dateb, :numpas, type: Array, default: []
---
+++ ruby_2.0/Telega_bot/train.rb
@@ -0,0 +1,23 @@
+class Train
+  include Mongoid::Document
+  field :number, type: String
+  field :type, type: String
+  field :wagons, type: Array, default: []
+  field :route, type: String
+
+  validates :number, presence: true
+  validates :type, presence: true
+
+  index ({number: 1}, {unique: true})
+
+  def approve_route(route_id)
+    station = Station.where(id: route.stations.first).first #метод where относится к ActiveRecord
+    raise SearchError, 'Station Not Found' unless station 
+    
+    route = Route.where(id: route_id).first
+    raise SearchError, 'Route Not Found' unless route
+
+    update_attributes(route: route.id.to_s)
+    station.accept(id.to_s) #tcp ruby
+  end
+end
